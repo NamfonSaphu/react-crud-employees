db6113636045f5444034e32fac9c995672853439
NamfonSaphu <namfonsaphu@gmail.com>
2024-05-02 15:04:13 +0700

    add employee success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
 import Axios from 'axios'
+import { count } from 'firebase/firestore/lite';
 import { useState } from 'react'
 
 function App() {
+
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const [country, setCountry] = useState("");
+    const [position, setPosition] = useState("");
+    const [wage, setWage] = useState(0);
 
   const [employeeList, setEmployeesList] = useState([]);
 
@@ -13,6 +20,26 @@ function App() {
     }); 
   }
 
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        wage: wage
+    }).then(() => {
+        setEmployeesList([
+            ...employeeList, {
+                name: name,
+                age: age,
+                country: country,
+                position: position,
+                wage: wage
+            }
+        ])
+    })
+  }
+
   return (
     <div className="App container">
       <h1>Employee Information</h1>
@@ -20,31 +47,30 @@ function App() {
         <form action="">
           <div className="mb-3">
             <label htmlFor="name" className="form-label">Name:</label>
-            <input type="text" className="form-control" placeholder="Enter name" />
+            <input type="text" className="form-control" placeholder="Enter name" onChange={(event) => {setName(event.target.value)}}/>
           </div>
           <div className="mb-3">
             <label htmlFor="age" className="form-label">Age:</label>
-            <input type="number" className="form-control" placeholder="Enter age" />
+            <input type="number" className="form-control" placeholder="Enter age" onChange={(event) => {setAge(event.target.value)}}/>
           </div>
           <div className="mb-3">
             <label htmlFor="country" className="form-label">Country:</label>
-            <input type="text" className="form-control" placeholder="Enter country" />
+            <input type="text" className="form-control" placeholder="Enter country" onChange={(event) => {setCountry(event.target.value)}}/>
           </div>
           <div className="mb-3">
             <label htmlFor="position" className="form-label">Position:</label>
-            <input type="text" className="form-control" placeholder="Enter position" />
+            <input type="text" className="form-control" placeholder="Enter position" onChange={(event) => {setPosition(event.target.value)}}/>
           </div>
           <div className="mb-3">
             <label htmlFor="wage" className="form-label">Wage:</label>
-            <input type="number" className="form-control" placeholder="Enter wage" />
+            <input type="number" className="form-control" placeholder="Enter wage" onChange={(event) => {setWage(event.target.value)}}/>
           </div>
-          <button className='btn btn-success'>Add Employee</button>
+          <button className='btn btn-success' onClick={addEmployee}>Add Employee</button>
         </form>
       </div>
       <hr />
       <div className="employee">
         <button className='btn btn-primary' onClick={getEmployees}>Show Employee</button>
-        <hr />
         {employeeList.map((val, key)=>{
           return (
             <div className="employee card">
